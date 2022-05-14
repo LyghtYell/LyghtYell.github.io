acff59d766e5e8bc926abeeb7ac176da3e25a5e7
LyghtYell <105540188+LyghtYell@users.noreply.github.com>
2022-05-14 18:32:35 +1000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,155 @@
+let firstName = '♡holow knight♡';
+console.log(firstName);
+
+let buttonElement = document.querySelector('#lore_dragon');
+console.log(buttonElement);
+
+let Elementbutton = document.querySelector('#HellMake');
+console.log(Elementbutton);
+
+let Subnatica = document.querySelector('#logitech');
+console.log(Elementbutton);
+
+let Lipton = document.querySelector('#Lipton');
+console.log(Elementbutton);
+
+let pool = document.querySelector('#pool');
+console.log(Elementbutton);
+
+let pepsi = document.querySelector('#pepsi');
+console.log(Elementbutton);
+
+let coca = document.querySelector('#coca');
+console.log(Elementbutton);
+
+let Dota = document.querySelector('#Dota');
+console.log(Elementbutton);
+
+let lol = document.querySelector('#lol');
+console.log(Elementbutton);
+
+let cs = document.querySelector('#cs');
+console.log(Elementbutton);
+
+let go = document.querySelector('#go');
+console.log(Elementbutton);
+
+let Valorant = document.querySelector('#Valorant');
+console.log(Elementbutton);
+
+let filin = document.querySelector('#filin');
+console.log(Elementbutton);
+
+let sova = document.querySelector('#sova');
+console.log(Elementbutton);
+
+let player = document.querySelector('#player');
+console.log(Elementbutton);
+
+let mouse = document.querySelector('#mouse');
+console.log(Elementbutton);
+
+let mise = document.querySelector('#mise');
+console.log(Elementbutton);
+
+buttonElement.addEventListener('click', function () {
+    let color = '#FFDFDC';
+    navigator.clipboard.writeText(color);
+    alert(`Цвет ${color} скопировать?`);
+});
+
+Elementbutton.addEventListener('click', function () {
+    let color = '#F6DDDF';
+    navigator.clipboard.writeText(color);
+    alert(`Цвет ${color} скопировать? (если да нажмите"ОК")`);
+});
+
+Subnatica.addEventListener('click', function () {
+    let color = '#DCABAE';
+    navigator.clipboard.writeText(color);
+    alert(`Цвет ${color} скопировать? (если да нажмите"ОК") `);
+});
+
+Lipton.addEventListener('click', function () {
+    let color = '#FF9CA1';
+    navigator.clipboard.writeText(color);
+    alert(`Цвет ${color} скопировать? (если да нажмите"ОК") `);
+});
+
+pool.addEventListener('click', function () {
+    let color = '#DB6B88';
+    navigator.clipboard.writeText(color);
+    alert(`Цвет ${color} скопировать? (если да нажмите"ОК") `);
+});
+
+pepsi.addEventListener('click', function () {
+    let color = '#FF7272';
+    navigator.clipboard.writeText(color);
+    alert(`Цвет ${color} скопировать? (если да нажмите"ОК") `);
+});
+
+coca.addEventListener('click', function () {
+    let color = '#E85668';
+    navigator.clipboard.writeText(color);
+    alert(`Цвет ${color} скопировать? (если да нажмите"ОК") `);
+});
+
+Dota.addEventListener('click', function () {
+    let color = '#FFC46B';
+    navigator.clipboard.writeText(color);
+    alert(`Цвет ${color} скопировать? (если да нажмите"ОК") `);
+});
+
+lol.addEventListener('click', function () {
+    let color = '#FFAF50';
+    navigator.clipboard.writeText(color);
+    alert(`Цвет ${color} скопировать? (если да нажмите"ОК") `);
+});
+
+cs.addEventListener('click', function () {
+    let color = '#FC9A40';
+    navigator.clipboard.writeText(color);
+    alert(`Цвет ${color} скопировать? (если да нажмите"ОК") `);
+});
+
+go.addEventListener('click', function () {
+    let color = '#FF905A';
+    navigator.clipboard.writeText(color);
+    alert(`Цвет ${color} скопировать? (если да нажмите"ОК") `);
+});
+
+Valorant.addEventListener('click', function () {
+    let color = '#FFAD32';
+    navigator.clipboard.writeText(color);
+    alert(`Цвет ${color} скопировать? (если да нажмите"ОК") `);
+});
+
+filin.addEventListener('click', function () {
+    let color = '#E5F0FF';
+    navigator.clipboard.writeText(color);
+    alert(`Цвет ${color} скопировать? (если да нажмите"ОК") `);
+});
+
+sova.addEventListener('click', function () {
+    let color = '#B7D4FF';
+    navigator.clipboard.writeText(color);
+    alert(`Цвет ${color} скопировать? (если да нажмите"ОК") `);
+});
+
+player.addEventListener('click', function () {
+    let color = '#7EB3FF';
+    navigator.clipboard.writeText(color);
+    alert(`Цвет ${color} скопировть? (если да нажмите"ОК") `);
+});
+
+mouse.addEventListener('click', function () {
+    let color = '#51EAFF';
+    navigator.clipboard.writeText(color);
+    alert(`Цвет ${color} скопировать? (если да нажмите"ОК") `);
+});
+
+mise.addEventListener('click', function () {
+    let color = '#51EAFF';
+    navigator.clipboard.writeText(color);
+    alert(`Цвет ${color} скопировать? (если да нажмите"ОК") `);
+});
